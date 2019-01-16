a8587f26916f586b52b3a16edce2f28642379352
Olivia Lee <olivia.suji.lee@berkeley.edu>
2019-01-16 03:38:23 -0800

    initial skeleton to new careers page

--- src/containers/About/Careers/Careers.jsx
+++ src/containers/About/Careers/Careers.jsx
@@ -6,53 +6,238 @@ import styled from 'styled-components';
 
 // components
 import { PageHeader, SectionHeader } from '../../components/HeaderStyles';
-import CareersTable from './CareersTable';
+import { RowContainer, ColumnContainer } from '../../components/ContainerStyles';
+import { ParaText } from '../../components/TextStyles';
 import Loader from '../../components/Loader';
-import FULLTIME_COLUMNS from '../../../constants/fulltimeColumns';
 
 // actions
 import { getFulltimes } from '../../../actions/data';
 
-class Careers extends Component {
-  componentDidMount() {
-    document.title = 'Careers - Pi Sigma Epsilon | Zeta Chi Chapter';
-    this.getFulltimes();
-  }
+const Careers = () => {
+  document.title = 'Careers - Pi Sigma Epsilon | Zeta Chi Chapter';
 
-  getFulltimes = async () => await this.props.getFulltimes();
+  return (
+    <div id="careers-container">
+      <RowContainer>
+        <Image
+          src="http://res.cloudinary.com/berkeleypse-tech/image/upload/f_auto,fl_force_strip,q_auto:best/campanile"
+          alt="Pi Sigma Epsilon brotherhood"
+        />
+        <Header className="py-2 px-0" altStyle>
+          Careers
+        </Header>
+      </RowContainer>
+      <DataContainer>
+        <NavYearBar>
+          <ul><strong>2019</strong></ul>
+          <ul>2018</ul>
+          <ul>2017</ul>
+          <ul>2016</ul>
+          <ul>2015</ul>
+        </NavYearBar>
 
-  render() {
-    const { fulltimes } = this.props;
-    return (
-      <div id="careers-container">
-        <PageHeader>Careers</PageHeader>
-        {fulltimes.loading ? (
-          <Loader />
-        ) : (
-          <TableContainer className="m-0 mb-2 p-0">
-            <SectionHeader altStyle>Full-Time</SectionHeader>
-            <CareersTable
-              id="fulltime-table"
-              columns={FULLTIME_COLUMNS}
-              data={fulltimes.data}
-            />
-          </TableContainer>
-        )}
-      </div>
-    );
-  }
-}
+        <CareerHeader altStyle>Full-Time</CareerHeader>
+        <TableContainer className="mb-0 pb-1">
+          <Label>
+            <Na>NAME</Na>
+            <Co>COMPANY</Co> 
+            <Gr>GROUP</Gr>
+            <Lo>LOCATION</Lo>
+          </Label>
+          <Industry>MARKETING</Industry>
+          <Row>
+            <Na>Yixuan Lu</Na>
+            <Co>IBM</Co>
+            <Gr>Programmatic Marketing</Gr>
+            <Lo>New York, NY</Lo>
+          </Row>
+          <Industry>CONSULTING</Industry>
+          <Row>
+            <Na>Linda Li</Na>
+            <Co>Accenture</Co>
+            <Gr>Management Consulting</Gr>
+            <Lo>San Francisco, CA</Lo>
+          </Row>
+          <Industry>FINANCE</Industry>
+          <Row>
+            <Na>Kevin Lee</Na>
+            <Co>PwC</Co>
+            <Gr>Tax</Gr>
+            <Lo>San Francisco, CA</Lo>
+          </Row>
+          <Row>
+            <Na>Kyle Lu</Na>
+            <Co>KPMG</Co>
+            <Gr>Business Valuations</Gr>
+            <Lo>San Francisco, CA</Lo>
+          </Row>
+        </TableContainer>
 
-const mapStateToProps = state => ({
-  fulltimes: state.data.fulltimes,
-  internship: state.data.internships
-});
+        <CareerHeader altStyle>Internships</CareerHeader>
+        <TableContainer>
+          <Label>
+            <Na>NAME</Na>
+            <Co>COMPANY</Co> 
+            <Gr>GROUP</Gr>
+            <Lo>LOCATION</Lo>
+          </Label>
+          <Industry>MARKETING</Industry>
+          <Row>
+            <Na>Rose Min</Na>
+            <Co>P&G</Co>
+            <Gr>Sales</Gr>
+            <Lo>-</Lo>
+          </Row>
+          <Row>
+            <Na>Vivan Hong</Na>
+            <Co>IBM</Co>
+            <Gr>Solutions Sales Summit Program</Gr>
+            <Lo>-</Lo>
+          </Row>
+          <Industry>CONSULTING</Industry>
+          <Row>
+            <Na>Catie Fan</Na>
+            <Co>Bain & Company</Co>
+            <Gr>Management Consulting</Gr>
+            <Lo>San Francisco, CA</Lo>
+          </Row>
+          <Row>
+            <Na>Clevian Hsia</Na>
+            <Co>PwC</Co>
+            <Gr>Forensic Technology Solutions</Gr>
+            <Lo>San Francisco, CA</Lo>
+          </Row>
+          <Row>
+            <Na>Kian Kiasaleh</Na>
+            <Co>Deloitte</Co>
+            <Gr>Human Capital Consulting</Gr>
+            <Lo>New York, NY</Lo>
+          </Row>
+          <Row>
+            <Na>Varsha Sundar</Na>
+            <Co>Accenture</Co>
+            <Gr>Management Consulting</Gr>
+            <Lo>San Francisco, CA</Lo>
+          </Row>
+          <Industry>FINANCE</Industry>
+          <Row>
+            <Na>Cathy An</Na>
+            <Co>Abbott</Co>
+            <Gr>-</Gr>
+            <Lo>-</Lo>
+          </Row>
+          <Row>
+            <Na>Gabriel Duarte</Na>
+            <Co>Deloitte</Co>
+            <Gr>Pioneer Internship</Gr>
+            <Lo>San Francisco, CA</Lo>
+          </Row>
+          <Row>
+            <Na>Harika Kalluri</Na>
+            <Co>AllianceBernstein</Co>
+            <Gr>Equity Research</Gr>
+            <Lo>San Francisco, CA</Lo>
+          </Row>
+          <Row>
+            <Na>Andrew Zhou</Na>
+            <Co>BDO</Co>
+            <Gr>Tax</Gr>
+            <Lo>San Francisco, CA</Lo>
+          </Row>
+          <Row className="mb-1q">
+            <Na>Liona Li</Na>
+            <Co>Wells Fargo</Co>
+            <Gr>Consumer Lending Intern</Gr>
+            <Lo>Sioux Falls, SD</Lo>
+          </Row>
+        </TableContainer>
+      </DataContainer>
+    </div>
+  );
+};
 
-export default connect(
-  mapStateToProps,
-  { getFulltimes }
-)(Careers);
+export default Careers;
+
+const Image = styled.img`
+  position: relative;
+  height:60vh;
+  width:100vw;
+  object-fit:cover;
+`;
+
+const Header = PageHeader.extend`
+  font-size: 3rem;
+  text-align: center;
+  position: absolute;
+  width: 100%;
+`;
+
+const InfoText = ParaText.extend`
+  margin: 1rem;
+`;
 
 const TableContainer = styled.div`
-  overflow-x: scroll;
+  width:80vw;
+  padding: 0.75rem 1rem 1.5rem 0rem;
+  text-align: left;
+  color: ${props => (props.altStyle ? '#FFF' : '#333')};
+  font-size: 1.125rem;
+  margin: 0.25rem 0;
+`;
+
+const DataContainer = ColumnContainer.extend`
+  width:80vw;
+  margin:auto;
+  align-items:flex-start;
+`;
+
+const NavYearBar = RowContainer.extend`
+  position:relative;
+  left:65%;
+  padding-top:20px;
+`;
+
+const CareerHeader = SectionHeader.extend`
+  position:relative
+  padding-bottom:0.5rem;
+`;
+
+const Row = RowContainer.extend`
+  justify-content:space-between;
+  text-align:left;
+  font-size:0.9rem;
+  padding:0.4rem 0.5rem;
+  border-style:solid none solid none;
+  border-width: 0.5px;
+  border-color:rgb(51,51,51,0.5);
+`;
+
+const Label = Row.extend`
+  margin-bottom:0.5rem;
+  font-weight:600;
+  opacity:0.5;
+  font-size:0.85rem;
+  border:none;
+`;
+
+const Industry = Row.extend`
+  font-size:0.9rem;
+  font-weight:600;
+  background-color: rgb(51,51,51,0.1)
+`;
+
+const Na = styled.div`
+  width:15%;
+`;
+
+const Co = styled.div`
+  width:25%;
+`;
+
+const Gr = styled.div`
+  width:35%;
+`;
+
+const Lo = styled.div`
+  width:15%;
 `;
--- src/containers/About/Careers/CareersTable.jsx
+++ src/containers/About/Careers/CareersTable.jsx
@@ -4,52 +4,140 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const CareersTable = props => {
-  const columnHeaders = (
-    <Row className="m-0">
-      {Object.values(props.columns).map((col, index) => {
-        return (
-          <ColumnHeader
-            className="m-0 py-0q px-0"
-            key={`${props.id}_col_${index}`}
-          >
-            {col}
-          </ColumnHeader>
-        );
-      })}
-    </Row>
-  );
+//table module imports
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-  const tableRows = props.data.map((person, index) => {
-    return (
-      <Row className="m-0" key={`${props.id}_row_${index}`} index={index}>
-        {Object.keys(props.columns).map((col, index) => {
-          return (
-            <Cell
-              className="m-0 py-0h px-0q"
-              key={`${props.id}_${person.name}_${col}`}
-            >
-              {person[col] || ''}
-            </Cell>
-          );
-        })}
-      </Row>
-    );
-  });
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+  row: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+});
+
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+function CustomizedTable(props) {
+  const { classes } = props;
 
   return (
-    <Table cellPadding="0" cellSpacing="0">
-      <tbody>
-        {columnHeaders}
-        {tableRows}
-      </tbody>
-    </Table>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <CustomTableCell>Dessert (100g serving)</CustomTableCell>
+            <CustomTableCell align="right">Calories</CustomTableCell>
+            <CustomTableCell align="right">Fat (g)</CustomTableCell>
+            <CustomTableCell align="right">Carbs (g)</CustomTableCell>
+            <CustomTableCell align="right">Protein (g)</CustomTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow className={classes.row} key={row.id}>
+              <CustomTableCell component="th" scope="row">
+                {row.name}
+              </CustomTableCell>
+              <CustomTableCell align="right">{row.calories}</CustomTableCell>
+              <CustomTableCell align="right">{row.fat}</CustomTableCell>
+              <CustomTableCell align="right">{row.carbs}</CustomTableCell>
+              <CustomTableCell align="right">{row.protein}</CustomTableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   );
+}
+
+CustomizedTable.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
 
-export default CareersTable;
+export default withStyles(styles)(CustomizedTable);
 
-const Table = styled.table`
+// const CareersTable = props => {
+//   const columnHeaders = (
+//     <Row className="m-0">
+//       {Object.values(props.columns).map((col, index) => {
+//         return (
+//           <ColumnHeader
+//             className="m-0 py-0q px-0"
+//             key={`${props.id}_col_${index}`}
+//           >
+//             {col}
+//           </ColumnHeader>
+//         );
+//       })}
+//     </Row>
+//   );
+
+//   const tableRows = props.data.map((person, index) => {
+//     return (
+//       <Row className="m-0" key={`${props.id}_row_${index}`} index={index}>
+//         {Object.keys(props.columns).map((col, index) => {
+//           return (
+//             <Cell
+//               className="m-0 py-0h px-0q"
+//               key={`${props.id}_${person.name}_${col}`}
+//             >
+//               {person[col] || ''}
+//             </Cell>
+//           );
+//         })}
+//       </Row>
+//     );
+//   });
+
+//   return (
+//     <CTable cellPadding="0" cellSpacing="0">
+//       <tbody>
+//         {columnHeaders}
+//         {tableRows}
+//       </tbody>
+//     </CTable>
+//   );
+// };
+
+// export default CareersTable;
+
+const CTable = styled.table`
   border: none;
   border-top: 1px solid #eee;
   width: 100%;
@@ -79,11 +167,11 @@ const Cell = styled.td`
 
 // PropTypes
 
-CareersTable.propTypes = {
-  columns: PropTypes.objectOf(PropTypes.string),
-  data: PropTypes.arrayOf(PropTypes.object)
-};
+// CareersTable.propTypes = {
+//   columns: PropTypes.objectOf(PropTypes.string),
+//   data: PropTypes.arrayOf(PropTypes.object)
+// };
 
-Row.propTypes = {
-  index: PropTypes.number
-};
+// Row.propTypes = {
+//   index: PropTypes.number
+// };
